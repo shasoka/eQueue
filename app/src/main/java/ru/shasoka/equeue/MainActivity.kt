7e69c04838aeb95fc0a48c31241923d77a4a3c05
Arkady Schoenberg <shasoka@yandex.ru>
2024-07-15 20:11:35 +0800

    Added SibFU on-startup splash screen.

--- app/src/main/java/ru/shasoka/equeue/MainActivity.kt
+++ app/src/main/java/ru/shasoka/equeue/MainActivity.kt
@@ -8,27 +8,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ru.shasoka.equeue.ui.theme.EQueueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             EQueueTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
             }
         }
     }
