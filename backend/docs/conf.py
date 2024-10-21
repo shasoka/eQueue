b790b1171f7e0db6f46df3595661cbb4406d9c62
shasoka <shasoka@yandex.ru>
2024-10-21 20:55:55 +0700

    Fixed GitHub Pages deployment action.

--- backend/docs/conf.py
+++ backend/docs/conf.py
@@ -10,7 +10,8 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.abspath("../eQueue"))
+if sys.platform == "win32":
+    sys.path.insert(0, os.path.abspath("../eQueue"))
 
 project = "eQueue API"
 copyright = "Copyright (c) YYYY Arkady Schoenberg <shasoka@yandex.ru>"
