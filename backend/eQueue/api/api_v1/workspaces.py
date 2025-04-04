b6c1a77002b2fc4551e3c63e3a0187d4324f8a42
Arkady Schoenberg <shasoka@yandex.ru>
2025-04-04 19:15:55 +0700

    Added method for getting single workspace by id.

--- backend/eQueue/api/api_v1/workspaces.py
+++ backend/eQueue/api/api_v1/workspaces.py
@@ -24,137 +24,156 @@ from crud.workspaces import (
     leave_workspace,
     workspace_safe_delete,
     raise_user,
+    get_workspace_by_id,
 )
 from moodle.auth import (
     get_current_user,
 )
 
 router = APIRouter(
-    tags=["Workspaces"],
+	tags=["Workspaces"],
 )
+
+
+@router.get("/{workspace_id}", response_model=WorkspaceRead)
+async def get_single_workspace(
+		workspace_id: int,
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		_: Annotated[User, Depends(get_current_user)],
+):
+	if workspace := await get_workspace_by_id(
+			workspace_id=workspace_id,
+			session=session,
+	):
+		return workspace
+	raise HTTPException(
+		404, detail="Не найдено ни одно рабочее пространство c таким id"
+	)
 
 
 @router.get("", response_model=list[WorkspaceRead])
 async def get_exisiting_workspace(
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if workspace := await get_available_workspace(session=session, user=current_user):
-        return workspace
-    raise HTTPException(404, detail="Не найдено ни одно рабочее пространство")
+	if workspace := await get_available_workspace(session=session,
+	                                              user=current_user):
+		return workspace
+	raise HTTPException(404, detail="Не найдено ни одно рабочее пространство")
 
 
 @router.post("", response_model=WorkspaceReadNoSelectInLoad)
 async def create_new_workspace(
-    workspace_in: WorkspaceCreate,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		workspace_in: WorkspaceCreate,
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if current_user.assigned_group_id != workspace_in.group_id:
-        raise HTTPException(
-            status_code=403,
-            detail="Вы не можете создать рабочее пространство в этой группе",
-        )
-    if workspace := await create_workspace(
-        workspace_in=workspace_in, session=session, user=current_user
-    ):
-        return workspace
-    raise HTTPException(
-        409, detail="Нарушено ограничение на создание рабочего пространства"
-    )
+	if current_user.assigned_group_id != workspace_in.group_id:
+		raise HTTPException(
+			status_code=403,
+			detail="Вы не можете создать рабочее пространство в этой группе",
+		)
+	if workspace := await create_workspace(
+			workspace_in=workspace_in, session=session, user=current_user
+	):
+		return workspace
+	raise HTTPException(
+		409, detail="Нарушено ограничение на создание рабочего пространства"
+	)
 
 
 @router.patch("", response_model=WorkspaceReadNoSelectInLoad)
 async def update_workspace_data(
-    workspace_upd: WorkspaceUpdate,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		workspace_upd: WorkspaceUpdate,
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if not current_user.assigned_workspace_id or not current_user.workspace_chief:
-        raise HTTPException(
-            status_code=403,
-            detail="Вы не можете обновить данные рабочего пространства",
-        )
-    if workspace := await update_workspace(
-        workspace_upd=workspace_upd,
-        session=session,
-        user=current_user,
-    ):
-        return workspace
-    raise HTTPException(
-        status_code=404,
-        detail="Не найдено ни одно рабочее пространство или пользователь уже подал заявку на вступление в группу",
-    )
+	if not current_user.assigned_workspace_id or not current_user.workspace_chief:
+		raise HTTPException(
+			status_code=403,
+			detail="Вы не можете обновить данные рабочего пространства",
+		)
+	if workspace := await update_workspace(
+			workspace_upd=workspace_upd,
+			session=session,
+			user=current_user,
+	):
+		return workspace
+	raise HTTPException(
+		status_code=404,
+		detail="Не найдено ни одно рабочее пространство или пользователь уже подал заявку на вступление в группу",
+	)
 
 
 @router.patch(settings.api.v1.join_workspace, response_model=WorkspaceRead)
 async def join_workspace(
-    workspace_upd: WorkspaceJoin,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		workspace_upd: WorkspaceJoin,
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    return await update_pending_users(
-        session=session,
-        workspace_upd=workspace_upd,
-        user=current_user,
-    )
+	return await update_pending_users(
+		session=session,
+		workspace_upd=workspace_upd,
+		user=current_user,
+	)
 
 
 @router.patch(settings.api.v1.raise_up_user, response_model=UserRead)
 async def make_user_workspace_chief(
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    user_id: int,
-    current_user: Annotated[User, Depends(get_current_user)],
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		user_id: int,
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    return await raise_user(session=session, user=current_user, user_id=user_id)
+	return await raise_user(session=session, user=current_user,
+	                        user_id=user_id)
 
 
 @router.post(settings.api.v1.accept_pending, response_model=UserRead)
 async def accept_join_request(
-    user_id: int,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		user_id: int,
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if not current_user.workspace_chief:
-        raise HTTPException(
-            status_code=403,
-            detail="Вы не можете подтвердить заявку на вступление в рабочее пространство",
-        )
-    return await accept_pending(
-        user_id=user_id,
-        session=session,
-        user=current_user,
-    )
+	if not current_user.workspace_chief:
+		raise HTTPException(
+			status_code=403,
+			detail="Вы не можете подтвердить заявку на вступление в рабочее пространство",
+		)
+	return await accept_pending(
+		user_id=user_id,
+		session=session,
+		user=current_user,
+	)
 
 
 @router.post(settings.api.v1.leave_workspace, response_model=UserRead)
 async def user_leave_workspace(
-    user_id: int,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		user_id: int,
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if current_user.id != user_id and not current_user.workspace_chief:
-        raise HTTPException(
-            status_code=403,
-            detail="Вы не можете исключать других участников рабочего пространства",
-        )
-    return await leave_workspace(
-        user_id=user_id,
-        session=session,
-    )
+	if current_user.id != user_id and not current_user.workspace_chief:
+		raise HTTPException(
+			status_code=403,
+			detail="Вы не можете исключать других участников рабочего пространства",
+		)
+	return await leave_workspace(
+		user_id=user_id,
+		session=session,
+	)
 
 
 @router.delete("", response_model=WorkspaceReadNoSelectInLoad)
 async def delete_workspace(
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    current_user: Annotated[User, Depends(get_current_user)],
+		session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+		current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if not current_user.assigned_workspace_id or not current_user.workspace_chief:
-        raise HTTPException(
-            status_code=403,
-            detail="Вы не можете удалить рабочее пространство",
-        )
-    return await workspace_safe_delete(
-        session=session,
-        user=current_user,
-    )
+	if not current_user.assigned_workspace_id or not current_user.workspace_chief:
+		raise HTTPException(
+			status_code=403,
+			detail="Вы не можете удалить рабочее пространство",
+		)
+	return await workspace_safe_delete(
+		session=session,
+		user=current_user,
+	)
